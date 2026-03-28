1c559890a6b6191577bfe77d24a5d50f0ca70e4b
enamnuel <ortegaenmanuel9w@gmail.com>
2026-03-27 19:37:12 -0600

    pantalla del provedor

--- Ferreteria Los Norteños S.A/proveedor.Designer.cs
+++ Ferreteria Los Norteños S.A/proveedor.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(proveedor));
+            this.SuspendLayout();
+            // 
+            // proveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "proveedor";
             this.Text = "proveedor";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
