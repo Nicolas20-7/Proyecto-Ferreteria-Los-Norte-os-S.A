0478cd1dbd32d8fbc08705950f39d45ea3fc12ca
Nicolas <sulyanneth88@gmail.com>
2026-04-08 15:21:43 -0600

    4/08/26

--- Ferreteria Los Norteños S.A/Form1.Designer.cs
+++ Ferreteria Los Norteños S.A/Form1.Designer.cs
@@ -47,6 +47,7 @@
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Name = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
@@ -58,8 +59,9 @@
             // btnNuevaventa
             // 
             resources.ApplyResources(this.btnNuevaventa, "btnNuevaventa");
-            this.btnNuevaventa.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnNuevaventa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(132)))), ((int)(((byte)(164)))));
             this.btnNuevaventa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNuevaventa.ForeColor = System.Drawing.Color.Transparent;
             this.btnNuevaventa.Name = "btnNuevaventa";
             this.btnNuevaventa.UseVisualStyleBackColor = false;
             this.btnNuevaventa.Click += new System.EventHandler(this.btnProductos_Click);
@@ -67,8 +69,9 @@
             // btnClientes
             // 
             resources.ApplyResources(this.btnClientes, "btnClientes");
-            this.btnClientes.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnClientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(132)))), ((int)(((byte)(164)))));
             this.btnClientes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClientes.ForeColor = System.Drawing.Color.Transparent;
             this.btnClientes.Name = "btnClientes";
             this.btnClientes.UseVisualStyleBackColor = false;
             this.btnClientes.Click += new System.EventHandler(this.button1_Click);
@@ -76,8 +79,9 @@
             // btnCaja
             // 
             resources.ApplyResources(this.btnCaja, "btnCaja");
-            this.btnCaja.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnCaja.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(132)))), ((int)(((byte)(164)))));
             this.btnCaja.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCaja.ForeColor = System.Drawing.Color.Transparent;
             this.btnCaja.Name = "btnCaja";
             this.btnCaja.UseVisualStyleBackColor = false;
             this.btnCaja.Click += new System.EventHandler(this.btnProveedores_Click);
@@ -85,8 +89,9 @@
             // btnProductos
             // 
             resources.ApplyResources(this.btnProductos, "btnProductos");
-            this.btnProductos.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnProductos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(132)))), ((int)(((byte)(164)))));
             this.btnProductos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProductos.ForeColor = System.Drawing.Color.Transparent;
             this.btnProductos.Name = "btnProductos";
             this.btnProductos.UseVisualStyleBackColor = false;
             this.btnProductos.Click += new System.EventHandler(this.button1_Click_1);
@@ -94,15 +99,16 @@
             // btnNuevacompra
             // 
             resources.ApplyResources(this.btnNuevacompra, "btnNuevacompra");
-            this.btnNuevacompra.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnNuevacompra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(132)))), ((int)(((byte)(164)))));
             this.btnNuevacompra.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNuevacompra.ForeColor = System.Drawing.Color.Transparent;
             this.btnNuevacompra.Name = "btnNuevacompra";
             this.btnNuevacompra.UseVisualStyleBackColor = false;
             this.btnNuevacompra.Click += new System.EventHandler(this.button2_Click);
             // 
             // ts
             // 
-            this.ts.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(108)))), ((int)(((byte)(164)))));
             this.ts.GripMargin = new System.Windows.Forms.Padding(3);
             this.ts.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.ts.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -150,7 +156,7 @@
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(19)))), ((int)(((byte)(58)))));
             this.Controls.Add(this.ts);
             this.Controls.Add(this.btnNuevacompra);
             this.Controls.Add(this.btnProductos);
