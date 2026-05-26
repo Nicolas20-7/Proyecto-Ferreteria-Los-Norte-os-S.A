60a4c3aa00ad56ad123bdf53b5dd0859ee73b29b
enamnuel <ortegaenmanuel9w@gmail.com>
2026-05-26 13:36:04 -0600

    cambios

--- Ferreteria Los Norteños S.A/nuevo usuario.Designer.cs
+++ Ferreteria Los Norteños S.A/nuevo usuario.Designer.cs
@@ -80,9 +80,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(268, 172);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.Size = new System.Drawing.Size(112, 20);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Contraseña 🔐";
+            this.label3.Text = "Contraseña :";
             // 
             // textBox3
             // 
@@ -120,7 +120,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(219, 231);
+            this.label5.Location = new System.Drawing.Point(16, 231);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(46, 20);
             this.label5.TabIndex = 10;
@@ -133,7 +133,7 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Gerente ",
             "Empleado"});
-            this.comboBox1.Location = new System.Drawing.Point(163, 254);
+            this.comboBox1.Location = new System.Drawing.Point(16, 254);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(184, 21);
             this.comboBox1.TabIndex = 12;
@@ -152,9 +152,9 @@
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(16, 172);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(93, 20);
+            this.label6.Size = new System.Drawing.Size(81, 20);
             this.label6.TabIndex = 13;
-            this.label6.Text = "Usuario 👤";
+            this.label6.Text = "Usuario :";
             // 
             // label1
             // 
