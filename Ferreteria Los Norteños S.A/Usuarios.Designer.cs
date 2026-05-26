c5c44fec3baec88d5286671a2887ce158e447f7c
enamnuel <ortegaenmanuel9w@gmail.com>
2026-05-26 15:26:50 -0600

    gf

--- Ferreteria Los Norteños S.A/Usuarios.Designer.cs
+++ Ferreteria Los Norteños S.A/Usuarios.Designer.cs
@@ -126,7 +126,7 @@
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.BackColor = System.Drawing.Color.Navy;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button2.Location = new System.Drawing.Point(12, 405);
@@ -139,7 +139,7 @@
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.Brown;
+            this.button3.BackColor = System.Drawing.Color.Navy;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button3.Location = new System.Drawing.Point(225, 405);
