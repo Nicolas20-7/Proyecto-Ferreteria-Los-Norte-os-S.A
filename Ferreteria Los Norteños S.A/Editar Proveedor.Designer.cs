06e583c1b334a875f0b37e6ae3a0c949ac18fe6d
enamnuel <ortegaenmanuel9w@gmail.com>
2026-05-26 13:04:54 -0600

    l,l

--- Ferreteria Los Norteños S.A/Editar Proveedor.Designer.cs
+++ Ferreteria Los Norteños S.A/Editar Proveedor.Designer.cs
@@ -91,9 +91,9 @@
             this.button1.BackColor = System.Drawing.Color.Green;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(216, 375);
+            this.button1.Location = new System.Drawing.Point(260, 374);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(330, 25);
+            this.button1.Size = new System.Drawing.Size(152, 59);
             this.button1.TabIndex = 25;
             this.button1.Text = "Guardar 💾";
             this.button1.UseVisualStyleBackColor = false;
